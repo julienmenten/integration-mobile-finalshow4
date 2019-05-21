c75ba9104beb4aef2875f537b5abefac3c7d8b20
Britt Leysen <britt.leysen@student.ehb.be>
2019-05-21 10:05:11 +0200

    aanpassingen filter, werkt nog niet

--- integration/www/js/filterBritt.js
+++ integration/www/js/filterBritt.js
@@ -1,54 +1,53 @@
 $(document).ready(function(){
-/*	$.ajax({
-		url:'json/projecten.json',
-		method: 'GET',
-		dataType: 'json'
-	}).done(function(data){
-		console.log('json');
-		for(var projecten in data){
-			var projectNaam = data.projects.naam;
-			var auteursNaam = data.projects.auteur;
-			
-			var divOpen= '<div><p>'
-			var divClose = '</p></div>'
-			var br = '<br>'
-			$('a').append(divOpen + projectNaam + br + 	auteursNaam + divClose);
-		}
-	});
-	*/
-	
+	// accordion structure	
 	$('#accordion').hide();
 	$('#filterButton').on('click', function(){
 		$('#accordion').toggle();
 		$( function() {
 			$( '#accordion' ).accordion({
-				"heightStyle":"content",
-				"selected":"false",
-				"collapsible":"true",
-    			"active":"false"
+				heightStyle:"content",
+				selected:"false",
+				collapsible:"true",
+    			active:"false"
 			});
 	
 		});
-		
-	/*	$('#eindwerk').on('click', function(){
-			$('#accordion').hide(300);
-			$.ajax({
-				url:"json/projectn.json",
-				method: "GET",
-				dataType: "json"
-			}).done(function(data){
-				for(var eindwerken in data){
-					var categorie = data.projects.categorie; 
-					var projectName = data.projects.naam;
-					
-					
-				} 
-				
-			});
-		})
-	*/
+	
 	});
+	
+	
+
 
 	
 		
-});
+	$.ajax({
+		url:"json/projectn.json",
+		method: "GET"
+	}).done(function(data){
+		for(var finalWorks in data){
+			var eindwerk = projects[data].categorie;
+			var categorie1 = projects[data].categorie;
+			
+			console.log('variabelen na call uitgevoerd');
+			
+			var eindwerkAfbeelding = $('#afbeeldingen').append('<p>' + eindwerk +'</p>');
+			console.log('eindwerkenAfbeelding aangemaakt');
+			var categorie1Afbeelding = $('#afbeeldingen').append('<p>'+categorie1+'</p>');
+			
+			if (eindwerk == 'eindwerken'){
+				console.log('eindwerken pressed');
+			// if pressed on #eindwerken show eindwerken
+				$('#eindwerk').on('click', function(){
+				$('#accordion').hide();
+				$('#afbeeldingen').hide();
+				$('#afbeeldingen').show(eindwerkAfbeelding);
+				
+			});
+			
+			//if (categorie1 == 'categorie1'){
+				
+			//}
+		};	 	
+	};
+	});
+	});
